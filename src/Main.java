b961e65b233d58646650960ecbed45e908aec76e
Darkdini921 <chigrin.00@mail.ru>
2025-02-10 16:56:04 +0300

    uppdate

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,11 @@ public class Main {
         System.out.println("Задача 1");
 
         int[] expenses = {50000, 55000, 52000, 54000, 51000};
+        int total2 = 0;
         for (int i = 0; i < expenses.length; i++) {
-            System.out.println("Сумма трат за месяц составила - " + expenses[i] + " рублей.");
+            total2 = expenses[i] + total2;
         }
+        System.out.println("Сумма трат за месяц составила - " + total2 + " рублей.");
 
         // Задача 2
 
@@ -25,7 +27,8 @@ public class Main {
             if (expenses2[i] > maxExpenses) {
                 maxExpenses = expenses2[i];
 
-            } if (expenses2[i] < minExpenses) {
+            }
+            if (expenses2[i] < minExpenses) {
                 minExpenses = expenses2[i];
             }
         }
@@ -37,12 +40,11 @@ public class Main {
         System.out.println("Задача 3");
 
         float[] expenses3 = {70000, 75000, 72000, 74000, 71000};
-        float numberOfWeeks = 4;
         float total = 0;
         for (float i = 0; i < expenses3.length; i++) {
             total = total + expenses3[(int) i];
         }
-        float averageAmount = total / numberOfWeeks;
+        float averageAmount = total / expenses3.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageAmount + " рублей");
 
         // Задача 4
@@ -50,9 +52,24 @@ public class Main {
         System.out.println("");
         System.out.println("Задача 4");
 
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--){
-            System.out.printf(String.valueOf(reverseFullName[i]));
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        StringBuilder firstName1  = new StringBuilder();
+        StringBuilder secondName1 = new StringBuilder();
+        boolean space = false;
+
+        for (char i : reverseFullName) {
+            if (i == ' '){
+                space = true;
+            } if (!space) {
+                firstName1.append(i);
+            } else {
+                secondName1.append(i);
+            }
         }
+        String firstName = new StringBuilder(firstName1).reverse().toString();
+        String secondName = new StringBuilder(secondName1).reverse().toString();
+
+        System.out.println(firstName + " " + secondName);
+
     }
 }
